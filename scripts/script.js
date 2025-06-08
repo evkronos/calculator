fb08fd9b2b2bc2f5a2f5fc13c4b10308e05c87bc
kronos <eliasvieiraresende23@gmail.com>
2025-06-08 12:34:13 -0300

    Added selectors for the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// funções para as operações
 function add(a, b) {
     return a + b;
 }
@@ -17,6 +18,26 @@ function divide(a, b) {
 // let n1 = prompt('Digite o primeiro número')
 // let operator = prompt('Escolha a operação')
 // let n2 = prompt('Digite o segundo número')
+
+//selecionar os botões
+const btnUm = document.getElementById('um')
+const btnDois = document.getElementById('dois')
+const btnTres = document.getElementById('tres')
+const btnQuatro = document.getElementById('quatro')
+const btnCinco = document.getElementById('cinco')
+const btnSeis = document.getElementById('seis')
+const btnSete = document.getElementById('sete')
+const btnOito = document.getElementById('oito')
+const btnNove = document.getElementById('nove')
+const btnZero = document.getElementById('zero')
+const addBtn = document.getElementById('add-btn')
+const subtractBtn = document.getElementById('subtract-btn')
+const multiplyBtn = document.getElementById('multiply-btn')
+const divideBtn = document.getElementById('divide-btn')
+const resultBtn = document.getElementById('result-btn')
+const clearBtn = document.getElementById('result-btn')
+
+// fazer array para armazenar os botões clicados pelo usuário e exibir no display
 
 function operate() {
     switch (operator) {
